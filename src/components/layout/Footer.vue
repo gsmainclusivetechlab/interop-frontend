51a34cc2ca1fab79b986289901864954ed8a44f5
Samy Bouchlaghem <sbouchlaghem@gsma.com>
2024-03-18 11:00:50 +0000

    Update for archival: add banner, remove request form

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -42,7 +42,7 @@
         </b-container>
       </div>
     </section>
-    <div class="new-case-wrapper w-100 ">
+    <!-- <div class="new-case-wrapper w-100 ">
       <div class="container">
         <div class="row">
           <div class="col-12">
@@ -67,7 +67,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <footer>
       <div class="footer-wrapper footer-bg">
@@ -133,7 +133,7 @@
                     <li>
                       <a href="/platform">How it works</a>
                     </li>
-                    <li><a href="/tryb4all">Try B4LL</a></li>
+                    <!-- <li><a href="/tryb4all">Try B4LL</a></li> -->
                     <li>
                       <a
                         href="https://docs.interop.gsmainclusivetechlab.io/"
